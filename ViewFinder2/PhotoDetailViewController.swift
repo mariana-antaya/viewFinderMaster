33754b8a07a49e6397d778ec78831d136765ca86
Apple <apple@apples-MBP-50.attlocal.net>
2019-07-16 11:25:01 -0700

    Completed Interation 3

---
+++ ViewFinder2/PhotoDetailViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PhotoDetailViewController.swift
+//  ViewFinder2
+//
+//  Created by Apple on 7/16/19.
+//  Copyright © 2019 Eric Smith. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+    
+    var photo : Photos? = nil
+    
+    @IBOutlet weak var photoDetailView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let realPhoto = photo {
+            title = realPhoto.caption
+
+            if let cellPhotoImageData = realPhoto.imageData {
+            
+            if let cellPhotoImage = UIImage(data : cellPhotoImageData) {
+        
+                photoDetailView.image = cellPhotoImage
+            }
+        }
+    }
+}
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
